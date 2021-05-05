c8cc3eb73329ef1b7ea1999af39b6865d5e3ccf8
LeonNepelius <leon@nepelius.at>
2021-05-05 09:42:55 +0200

    Finished

---
+++ CashRegister/CashRegister.Shared/ReceiptLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CashRegister.Shared
+{
+    public class ReceiptLine
+    {
+        [JsonPropertyName("id")]
+        public int ID { get; set; }
+
+        [JsonPropertyName("product")]
+        public Product Product { get; set; }
+
+        [JsonPropertyName("amount")]
+        public int Amount { get; set; }
+
+        [JsonPropertyName("totalPrice")]
+        public decimal TotalPrice { get; set; }
+    }
+
+    public class ReceiptLineDto
+    {
+        public int ProductID { get; set; }
+
+        public int Amount { get; set; }
+    }
+}
